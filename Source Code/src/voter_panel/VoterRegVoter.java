af64d6de6e3277722e967a39d9714292b9e3029e
R.Rana <55597540+ranaswe2@users.noreply.github.com>
2022-05-13 00:19:43 +0600

    Update VoterRegVoter.java

--- Source Code/src/voter_panel/VoterRegVoter.java
+++ Source Code/src/voter_panel/VoterRegVoter.java
@@ -301,7 +301,7 @@ public class VoterRegVoter extends javax.swing.JFrame implements AppInfo{
 
       Session session = Session.getInstance(properties, new javax.mail.Authenticator(){
         protected PasswordAuthentication getPasswordAuthentication() {
-          return new PasswordAuthentication("election.divs@gmail.com", "kjhtte8-04kiu8er");
+          return new PasswordAuthentication("election.divs@gmail.com", "********");
         }
       });
 
